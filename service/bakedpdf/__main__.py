2f0042144bd34ec3a381ace6fe34e36eeda29fbb
Krzysztof Mędrzycki <aiwenari@gmail.com>
2018-08-14 09:43:24 +0200

    Add skeleton for implementing commands

--- service/bakedpdf/__main__.py
+++ service/bakedpdf/__main__.py
@@ -1,10 +1,32 @@
 import argparse
 import toml
+from typing import Callable, Mapping
+
+from .config import Config
+
+
+Command = Callable[[Config, Mapping], None]
+
+
+COMMANDS: Mapping[str, Command] = {
+    'enqueue': enqueue,
+    'start': start,
+}
 
 
 def main():
     """Main console entry point"""
-    pass
+    argp = argparse.ArgumentParser()
+    argp.add_argument('-c', '--config', help="Load configuration file")
+    cmds = argp.add_subparsers(title='subcommands', dest='command')
+    args = argp.parse_args()
+
+    if args.config:
+        raise NotImplementedError()
+    else:
+        config = Config()
+
+    COMMANDS[args.command](config, args)
 
 
 if __name__ == '__main__':
