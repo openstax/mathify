f02a5421234943add5ae612767bd707ac12f150b
Tyler Nullmeier <tylerzeromaster@gmail.com>
2024-07-15 16:23:02 -0500

    When line numbering is enabled, substitute in the table directly
    Instead of keeping the code/pre tag surrounding it

--- typeset/converter.js
+++ typeset/converter.js
@@ -66,11 +66,11 @@ const createMapOfMathMLElements = async (log, inputPath, cssPath, outputPath, ou
   log.debug(`Opened "${inputPath}"`)
 
   const matchers = [
-    { attr: 'data-math' },
+    { attr: 'data-math' }
   ]
 
   if (outputFormat === 'html') {
-    matchers.push({tag: 'head'})
+    matchers.push({ tag: 'head' })
   }
 
   // I fear what might happen if we try to convert from mathml to mathml
@@ -82,12 +82,12 @@ const createMapOfMathMLElements = async (log, inputPath, cssPath, outputPath, ou
   if (highlight) {
     log.debug('Adding matchers for code highlighting...')
 
-    const tags = ['pre', 'code'];
-    const attributes = ['data-lang', 'lang'];
+    const tags = ['pre', 'code']
+    const attributes = ['data-lang', 'lang']
 
     for (let i = 0; i < tags.length; i++) {
       for (let j = 0; j < attributes.length; j++) {
-        matchers.push({ tag: tags[i], attr: attributes[j] });
+        matchers.push({ tag: tags[i], attr: attributes[j] })
       }
     }
   }
@@ -187,7 +187,7 @@ function getLanguage (el, attr) {
 
 async function highlightCodeElements (codeEntries) {
   codeEntries.forEach(entry => {
-    const el = parseXML(entry.element).documentElement
+    let el = parseXML(entry.element).documentElement
     // List of supported language classes: https://github.com/highlightjs/highlight.js/blob/master/SUPPORTED_LANGUAGES.md
     const language = getLanguage(el, 'data-lang') || getLanguage(el, 'lang')
     const hasLineNumbers = el.getAttribute('class').indexOf('line-numbering') !== -1
@@ -195,12 +195,16 @@ async function highlightCodeElements (codeEntries) {
     let outputHtml = hljs.highlight(language, inputCode).value
     if (hasLineNumbers) {
       outputHtml = hljsLineNumbers.addCodeLineNumbers(outputHtml)
+      el = parseXML(outputHtml).documentElement
+      Object.entries(entry.node.attributes).forEach(([k, v]) => {
+        el.setAttribute(k, v)
+      })
+    } else {
+      const newNode = parseXML(`<tempElement xmlns="http://www.w3.org/1999/xhtml">${outputHtml}</tempElement>`).documentElement
+      el.removeChild(el.firstChild)
+      Array.from(newNode.childNodes).forEach(child => el.appendChild(child))
     }
-    const newNode = parseXML(`<tempElement xmlns="http://www.w3.org/1999/xhtml">${outputHtml}</tempElement>`).documentElement
     const localNamespaces = Object.keys(entry.node.attributes).filter(k => k.startsWith('xmlns'))
-
-    el.removeChild(el.firstChild)
-    Array.from(newNode.childNodes).forEach(child => el.appendChild(child))
     entry.substitution = cleanNamespaces(el, localNamespaces)
   })
 }
