7fe98623c82d9da519e2823cc2937c036c820d37
Tyler Nullmeier <tylerzeromaster@gmail.com>
2026-03-27 13:39:04 -0500

    call converter directly

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 #!/usr/bin/env node
 
 const readline = require("readline");
-const { convertMathML } = require("./typeset/mjnode");
-
-const logger = new console.Console(process.stderr);
+const { TexToMml } = require("./typeset/converters");
 
 const main = async () => {
   const rl = readline.createInterface({ input: process.stdin });
+  const converter = new TexToMml({});
+  await converter.init();
 
   for await (const line of rl) {
     if (!line.trim()) continue;
     const batch = JSON.parse(line);
-    await convertMathML(
-      logger,
-      batch,
-      "mathml",
-      1000,
-      (pairs) => {
-        pairs.forEach(([message, entry]) => {
-          entry.error = message;
-        });
-      },
-      {},
-    );
+    const math = batch.map(({ mathSource }) => mathSource);
+    const results = await converter.convert(math, 1000);
+    results.forEach(({ math, error }, idx) => {
+      const entry = batch[idx];
+      if (error) {
+        entry.error = [error.message, error.source].join("\n");
+      } else if (math) {
+        entry.substitution = math.replace(/&nbsp;/g, "&#160;");
+      }
+    });
     process.stdout.write(
       JSON.stringify(
         batch.map(({ mathSource, substitution, error, ...rest }) => ({
